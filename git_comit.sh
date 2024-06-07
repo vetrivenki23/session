2d871480d13f29320c4b322ea946c2b9c4dc3eb9
Venki <techlearnings23@gmail.com>
2024-06-06 19:48:41 -0500

    TF script updated

--- git_comit.sh
+++ git_comit.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Prompt for commit description
-read -p "Enter commit description: " description
-
-# Git commands
-git add .
-git commit -m "$description"
-git branch -M main
-git push -u origin main
-
 # Prompt for confirmation before deleting .terraform* files and directories
 read -p "Do you want to delete .terraform* files and directories? (y/n): " confirm
 
@@ -23,3 +14,14 @@ if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
 else
     echo "Deletion of .terraform* files and directories skipped."
 fi
+
+
+# Prompt for commit description
+read -p "Enter commit description: " description
+
+# Git commands
+git add .
+git commit -m "$description"
+git branch -M main
+git push -u origin main
+
