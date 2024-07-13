e2a062f247c6bbc18fdb8d7dd6b776acf79802c6
Venki <techlearnings23@gmail.com>
2024-07-13 16:28:52 -0500

    updated

---
+++ Terraform/TF_Files/KK/15-EC2-Workspace/main.tf
@@ -0,0 +1,32 @@
+locals{
+instance_name = "${terraform.workspace}-instance" 
+}
+
+resource "aws_instance" "my-ec2" {
+  ami           = var.ec2_ami
+  instance_type = var.ec2_type
+  key_name = "us-east-2"
+  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+  user_data = file("userdata.sh")
+  
+  /* to copy a file from local to EC2 instance */
+  provisioner "file" {
+    /*source      = "C:\\Users\\kk198\\session\\Terraform\\TF_Files\\KK\\14-EC2-File Provisioner\\test-file.txt" */
+    content     = "I want to copy this string to the destination file server.txt"
+    destination = "content.txt"
+  }
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = file("C:\\Users\\kk198\\Downloads\\us-east-2.pem")
+      timeout     = "4m"
+   }
+
+  tags = {
+    Name = local.instance_name
+  }
+}
+
+
+
---
+++ Terraform/TF_Files/KK/15-EC2-Workspace/security.tf
@@ -0,0 +1,59 @@
+# Create Security Group - SSH Traffic
+resource "aws_security_group" "vpc-ssh" {
+  name        = "${terraform.workspace}-vpc-ssh"
+  description = "${terraform.workspace} VPC SSH"
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "vpc-ssh"
+  }
+}
+
+
+# Create Security Group - Web Traffic
+resource "aws_security_group" "vpc-web" {
+  name        = "${terraform.workspace}-vpc-web"
+  description = "${terraform.workspace} VPC Web"
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "vpc-web"
+  }
+}
